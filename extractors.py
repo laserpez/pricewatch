28a19c8702bcb48291cd344c3a7634e59039b10c
Simone Finotti <finotti.simone@fabbri1905.com>
2020-06-09 16:02:49 +0200

    First commit.

---
+++ extractors.py
@@ -0,0 +1,58 @@
+from price_parser import Price
+from urllib.parse import urlparse
+
+class ExtractNull():
+    def __call__(self, url, soup):
+        netloc = urlparse(url).netloc
+        raise NotImplementedError(f"domain {netloc} is not handled")
+
+class BaseExtractor:
+    def __init__(self, next):
+        self._next = next
+
+    def __call__(self, url, soup):
+        netloc = urlparse(url).netloc
+        return self.handle(url, soup) if netloc in self.domain() \
+          else self._next(url, soup)
+    
+    def handle(self, url, price):
+        raise NotImplementedError()
+
+class ExtractIsadore (BaseExtractor):
+    def domain(self): return {"isadore.com"}
+    
+    def handle(self, url, soup):
+        price_p = soup.find_all('span', attrs={'class': 'price'})[0]
+        price = Price.fromstring(price_p.get_text())
+        return price
+
+class ExtractLaPassione (BaseExtractor):
+    def domain(self): return {"www.lapassione.cc"}
+
+    def handle(self, url, soup):
+        price_p = soup.find("span", {"id": "ProductPrice"})
+        price = Price.fromstring(price_p.get_text())            
+        return price
+
+class ExtractScienceInSport (BaseExtractor):
+    def domain(self): return {"www.scienceinsport.com"}
+
+    def handle(self, url, soup):
+        pip = soup.find_all(attrs={'class': 'product-info-price'})[0]
+        price_p = pip.find_all('span', attrs={'data-price-type': 'finalPrice'})[0]
+        price = Price.fromstring(price_p.get_text())
+        return price
+
+class ExtractWiggle (BaseExtractor):
+    def domain(self): return {"www.wigglesport.it"}
+    
+    def handle(self, url, soup):
+        price_p = soup.find_all('p', attrs={'class': 'bem-pricing__product-price'})[0]
+        price = Price.fromstring(price_p.get_text())
+        return price
+
+extractor = ExtractScienceInSport(
+            ExtractWiggle(
+            ExtractIsadore(
+            ExtractLaPassione(
+            ExtractNull()))))
---
+++ main.py
@@ -0,0 +1,51 @@
+import argparse
+from bs4 import BeautifulSoup
+from decimal import Decimal
+from extractors import extractor
+from functools import partial
+import json
+import requests
+import smtplib
+import sys
+
+souper = partial(BeautifulSoup, features='html.parser')
+
+def compute_price(url, target, price_extractor):
+    soup = souper(requests.get(url).content)
+    price = price_extractor(url, soup)
+    return price
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Price tracker by laserpez.')
+    parser.add_argument('-m', '--mail', action='store_true', help='send e-mail instead of print')
+    parser.add_argument('-f', '--file', default='urls.json', help='URLs file path')
+    args = parser.parse_args()
+
+    with open(args.file, 'rt') as f:
+        input = json.load(f)
+        URLs = [(url, Decimal(price)) for url, price in input]
+
+    results = [(url, compute_price(url, target, extractor), target) for url, target in URLs]
+    send_report(results, send_mail=args.mail)
+    return 0
+
+def build_body(url, price, target):
+    delta = price.amount - target
+    body = f"Item at {url} price is still {price.amount}." if abs(delta) < 0.001 \
+      else f"Item at {url} price is now {price.amount} (difference: {delta:.2f})!"
+    
+    return body
+
+def send_report(results, send_mail=False):
+    with smtplib.SMTP_SSL('email-smtp.eu-central-1.amazonaws.com', 465) as server:
+        server.ehlo()
+        server.login("AKIAU2W3YTRDMFMDYIKC", "BOBzKJ5PyUMxT5mXCZd6m1OOg1oPCFJUTynSreWpD98R")
+      
+        subject = 'Pricewatch report'
+        body    = "\n\n".join(build_body(url, price, target) for url, price, target in results) + "\n"
+        msg     = f"Subject: {subject}\n\n{body}"
+
+        server.sendmail("\"Pricewatch\" <psycho_78@libero.it>", 'laserpez+cash@gmail.com', msg) if send_mail else print(msg)
+
+# entry point
+sys.exit(main(sys.argv[1:]) if __name__ == "__main__" else 0)
